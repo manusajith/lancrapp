01836c33b2314fd012a8b42b0ae9d7664b9e0f5d
Troy Martin <troy@devatotech.com>
2015-05-29 09:41:49 -0400

    updated accounts email verification

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,10 @@
 Accounts.emailTemplates.siteName = "Lancr";
 Accounts.emailTemplates.from = FROM_EMAIL;
 
+Accounts.config({
+  sendVerificationEmail: true
+});
+
 Accounts.onCreateUser(function(options, user) {
   if (options.profile)
     user.profile = options.profile;
